813f4bafa0e34a1acf580a37b000fe39744a0cde
danieltinetum <daniel.trejo@inetum.com>
2022-06-24 17:18:38 -0500

    R

--- index.js
+++ index.js
@@ -12,12 +12,19 @@ const main = async () => {
         'main',
         'master'
     ];
+    const errors = [];
+    const successful = [];
 
     const deployed_at = new Date().getTime();
 
     const { payload, ...context } = github.context;
     const branch = context.ref.substring(context.ref.lastIndexOf('/') + 1, context.ref.length);
     const remove_pull = payload.commits.filter(c => c.message.indexOf('pull request') === -1);
+
+    if (remove_pull.length === 0){
+        core.warning('There are no commits that comply with the evaluation.')
+        return;
+    }
 
     const { data } = await axios(createSearchConfig({
         api_key: core.getInput('api-key'),
@@ -33,8 +40,37 @@ const main = async () => {
     const { id, name } = data.applications.shift();
 
     for (const commits of chunk(remove_pull, PARTITION_SIZE)) {
-        console.log(commits.length, "/", payload.commits.length);
+        const requests = commits.map(commit =>
+            axios(createEventPostConfig({
+                "account_id": core.getInput('account-id'),
+                "insights_insert_key": core.getInput('insights-insert-key'),
+                "eventType": "BelDeployment",
+                "appId": id,
+                "appName": name,
+                "revision": context.runId,
+                "environment": PRD_BRANCHES.includes(branch) ? "PRD" : "QA",
+                "type": "regular",
+                "jobName": context.job,
+                "buildNumber": context.runNumber,
+                "branchName": branch,
+                "commit": commit.id,
+                "codeCommittedTime": new Date(commit.timestamp).getTime(),
+                "codeDeployedTime": deployed_at,
+                "buildStatus": "SUCESSFULL",
+                "sendFrom": "GithubActions"
+            }))
+        );
+
+        try {
+            await Promise.all(requests);
+            successful.push(requests.length);
+        } catch (error) {
+            errors.push(requests.length);
+        }
     }
+
+    core.setOutput("successful", successful.reduce((a, b) => a + b, 0));
+    core.setOutput("errors", errors.reduce((a, b) => a + b, 0));
 
 }
 
@@ -42,5 +78,6 @@ const main = async () => {
 try {
     main();
 } catch (error) {
-
+    core.setFailed(error.message);
+    process.exit(1);
 }
