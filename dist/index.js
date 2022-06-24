1833c72af783079709ee2f42d4f19fd476143fca
danieltinetum <daniel.trejo@inetum.com>
2022-06-23 23:27:52 -0500

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -13985,7 +13985,49 @@ const main = async () => {
     const application_id = data.applications[0].id;
     const nr_app_name = data.applications[0].name;
 
-    console.log( application_id, nr_app_name )
+    payload.commits.forEach(commit => {
+        const conf = JSON.parse(JSON.stringify(base))
+        conf.data = JSON.stringify({
+            "eventType": "BelDeployment",
+            "appId": application_id,
+            "appName": nr_app_name,
+            "revision": context.runId,
+            "environment": ['main', 'master'].includes(branch_name) ? "PRD" : "QA",
+            "type": "regular",
+            "jobName": context.job,
+            "buildNumber": context.runNumber,
+            "branchName": branch,
+            "commit": commit.id,
+            "codeCommittedTime": new Date(commit.timestamp).getTime(),
+            "codeDeployedTime": deployed_at,
+            "buildStatus": "SUCESSFULL",
+            "sendFrom": "GithubActions"
+        });
+        configs.push(
+            conf
+        );
+    })
+
+    for (const item of configs) {
+        try {
+            const result = await axios(item)
+            successful.push(result.data ? 1 : 0)
+        } catch (error) {
+            errors.push(1)
+        }
+    }
+
+    
+    core.setOutput("successful", successful.reduce((a, b) => a + b, 0));
+    core.setOutput("errors", errors.reduce((a, b) => a + b, 0));
+
+    if (errors.length === configs.length) {
+        core.setFailed(`All requests to New Relic were unsuccessful - (${errors.length})`);
+        process.exit(1);
+    }else if (errors.length > 0){
+        core.setFailed(`Some requests to New Relic were unsuccessful - ${errors.length} / ${configs.length}`);
+        process.exit(1);
+    }
 
 }
 
